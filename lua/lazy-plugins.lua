55d682b0fd4f27792ade32a806fb41369321e972
Anish <basketb523@gmail.com>
2024-05-26 22:29:30 +0545

    Kickstart-modular setup

---
+++ lua/lazy-plugins.lua
@@ -0,0 +1,67 @@
+
+require('lazy').setup({
+ 
+  'tpope/vim-sleuth', 
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  require 'kickstart/plugins/gitsigns',
+
+  require 'kickstart/plugins/which-key',
+
+  require 'kickstart/plugins/telescope',
+
+  require 'kickstart/plugins/lspconfig',
+
+  require 'kickstart/plugins/conform',
+
+  require 'kickstart/plugins/cmp',
+
+  require 'kickstart/plugins/tokyonight',
+
+  require 'kickstart/plugins/todo-comments',
+
+  require 'kickstart/plugins/mini',
+
+  require 'kickstart/plugins/treesitter',
+
+  require 'kickstart/plugins/harpoon',
+  require 'kickstart/plugins/oil',
+  require 'kickstart/plugins/dotnet',
+  require 'kickstart/plugins/undotree',
+  require 'kickstart/plugins/fugitive',
+
+  
+  require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+
+
+  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+  -- { import = 'custom.plugins' },
+}, {
+  ui = {
+    
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
