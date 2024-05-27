1bfbf3c200e6f8e1ff39c196633bce01d9da75f8
Anish <basketb523@gmail.com>
2024-05-28 01:03:25 +0545

    netcoredbg setup successfully for dotnet

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -3,16 +3,12 @@ return {
   'mfussenegger/nvim-dap',
 
   dependencies = {
-    -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
-
-    -- Required dependency for nvim-dap-ui
     'nvim-neotest/nvim-nio',
 
     -- Installs the debug adapters for you
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
-
     'theHamsta/nvim-dap-virtual-text',
     'nvim-telescope/telescope-dap.nvim',
 
@@ -22,43 +18,9 @@ return {
     local dap = require 'dap'
     local dapui = require 'dapui'
 
-    vim.g.dotnet_build_project = function()
-      local default_path = vim.fn.getcwd() .. '/'
-      if vim.g['dotnet_last_proj_path'] ~= nil then
-        default_path = vim.g['dotnet_last_proj_path']
-      end
-      local path = vim.fn.input('Path to your *proj file', default_path, 'file')
-      vim.g['dotnet_last_proj_path'] = path
-      local cmd = 'dotnet build -c Debug ' .. path .. ' > /dev/null'
-      print ''
-      print('Cmd to execute: ' .. cmd)
-      local f = os.execute(cmd)
-      if f == 0 then
-        print '\nBuild: ✔️ '
-      else
-        print('\nBuild: ❌ (code: ' .. f .. ')')
-      end
-    end
-
-    vim.g.dotnet_get_dll_path = function()
-      local request = function()
-        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
-      end
-
-      if vim.g['dotnet_last_dll_path'] == nil then
-        vim.g['dotnet_last_dll_path'] = request()
-      else
-        if vim.fn.confirm('Do you want to change the path to dll?\n' .. vim.g['dotnet_last_dll_path'], '&yes\n&no', 2) == 1 then
-          vim.g['dotnet_last_dll_path'] = request()
-        end
-      end
-
-      return vim.g['dotnet_last_dll_path']
-    end
-
     dap.adapters.coreclr = {
       type = 'executable',
-      command = 'C:\\Users\\Mangobyte-LTP-10\\netcoredbg\\netcoredbg.exe',
+      command = 'netcoredbg',
       args = { '--interpreter=vscode' },
     }
 
@@ -68,24 +30,12 @@ return {
         name = 'launch - netcoredbg',
         request = 'launch',
         program = function()
-          if vim.fn.confirm('Should I recompile first?', '&yes\n&no', 2) == 1 then
-            vim.g.dotnet_build_project()
-          end
-          return vim.g.dotnet_get_dll_path()
-        end,
-      },
-      {
-        type = 'coreclr',
-        name = 'attach - mywebsite',
-        request = 'attach',
-        processId = function()
-          return vim.fn.input 'Process Id '
-        end,
-        program = function()
-          return vim.fn.input('Path to dll', vim.fn.getcwd() .. '\\bin\\Debug\\', 'file')
+          return vim.fn.input('Path to dll ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
         end,
       },
     }
+
+    dap.set_log_level 'DEBUG'
 
     require('mason-nvim-dap').setup {
       automatic_installation = true,
@@ -93,8 +43,8 @@ return {
       handlers = {},
 
       ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        'netcoredbg',
+        -- -- Update this to ensure that you have the debuggers for the langs you want
+        -- 'netcoredbg',
       },
     }
 
